34208d1ff4fcaaa09afd8488ee6548a33310368f
Stephane Bellity <sbellity@gmail.com>
2013-06-04 09:02:12 -0600

    ...

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var express = require('express'),
     useHull = require('./lib/use_hull'),
     usePassport = require('./lib/use_passport');
 
+var config = {
+  appId:      "50cf17731fb4e935ea000001",
+  orgUrl:     "http://sbellity.hullapp.dev",
+  appSecret:  "c23d27fcf8c683c28a236c36d41dc457"
+};
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -10,9 +16,8 @@ app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.session({ secret: "Na na na, I'm secret!" }));
 
-useHull(app);
-usePassport(app);
-
+useHull(app, config);
+usePassport(app, config);
 
 app.use(app.router);
 
